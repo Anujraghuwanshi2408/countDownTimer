6428d03cbc128b40c17fae728b68679dbc7a8282
anuj raghuwanshi <anujonline46@gmail.com>
2023-07-12 12:09:13 +0530

    completed

--- script.js
+++ script.js
@@ -8,29 +8,29 @@ let result =setInterval( clock , () => {
 } , 1000)
 
 function clock(){
-    // const end = new Date(endDate);
-    // const now = new Date();
-
-    // const differenceInMs = end - now;
-    
-    // if(differenceInMs/1000 < 0){
-    //     return;
-    // }
-    // // no of days
-    // inputs[0].value = Math.floor(differenceInMs/(24*60*60*1000));
-    // inputs[1].value = Math.floor((differenceInMs/(60*60*1000))%24);
-    // inputs[2].value = Math.floor((differenceInMs/(1000*60))%60)
-    // // console.log(inputs[2].value );
-    // inputs[3].value = Math.floor(differenceInMs/(1000)%60);
-    
-    // // if(inputs[0].value != 0|| inputs[1].value !=0 || inputs[2].value !=0 || inputs[3].value !=0){
-    // //     clock();
-    // // }
-
     const end = new Date(endDate);
     const now = new Date();
 
-    inputs[0].value = Math.abs(now.getDate()-end.getDate());
+    const differenceInMs = end - now;
+    
+    if(differenceInMs/1000 < 0){
+        return;
+    }
+    // no of days
+    inputs[0].value = Math.floor(differenceInMs/(24*60*60*1000));
+    inputs[1].value = Math.floor((differenceInMs/(60*60*1000))%24);
+    inputs[2].value = Math.floor((differenceInMs/(1000*60))%60)
+    // console.log(inputs[2].value );
+    inputs[3].value = Math.floor(differenceInMs/(1000)%60);
+    
+    // if(inputs[0].value != 0|| inputs[1].value !=0 || inputs[2].value !=0 || inputs[3].value !=0){
+    //     clock();
+    // }
+
+    // const end = new Date(endDate);
+    // const now = new Date();
+
+    // inputs[0].value = Math.abs(now.getDate()-end.getDate());
    
 
     
